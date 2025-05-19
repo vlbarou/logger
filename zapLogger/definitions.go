d47b285717d910bb1546a668d64008ec81cce995
vlbarou <vlasisbar@gmail.com>
2025-05-19 19:01:28 +0300

    sdf

--- zapLogger/definitions.go
+++ zapLogger/definitions.go
@@ -7,7 +7,7 @@ const (
 	MaxBackups              = 3
 	MaxAge                  = 7
 	Compress                = true
-	LogFile                 = "/logs/app.log"
+	LogFile                 = "/tmp/logs/app.log"
 	TimeKey                 = "timestamp"
 	LoggerServerPort        = "8081"
 	GracefulShutdownTimeout = 5 * time.Second
